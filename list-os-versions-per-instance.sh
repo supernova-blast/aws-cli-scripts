d0500be22402ecf7656475fd2deccffd6f8c1eb1
Mitja <whatupdoc@yandex.com>
2025-09-17 16:53:05 +0400

    create list-os-versions-per-instance.sh

---
+++ list-os-versions-per-instance.sh
@@ -0,0 +1,26 @@
+# This script shows the OS version for all instances in the current region.
+# You could simply use the command below but it will not show the instance names, only instance-ids:
+# `aws ssm describe-instance-information --query \ 
+# "InstanceInformationList[].{InstanceId:InstanceId,PlatformType:PlatformType,PlatformName:PlatformName,PlatformVersion:PlatformVersion}" --output table`
+# So we are combining the output from the two queries here to show both the OS version and the corresponding instance name.
+
+#!/bin/bash
+
+# create a temporary file for the data
+temp_file=$(mktemp)
+
+# add header
+echo -e "InstanceId\tName\tPlatformType\tPlatformName\tPlatformVersion" > "$temp_file"
+
+# get data
+aws ssm describe-instance-information --query "InstanceInformationList[].[InstanceId,PlatformType,PlatformName,PlatformVersion]" --output text | \
+while read instance_id platform_type platform_name platform_version; do
+  name=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id" "Name=key,Values=Name" --query "Tags[0].Value" --output text 2>/dev/null)
+  echo -e "$instance_id\t${name:-"(no name)"}\t$platform_type\t$platform_name\t$platform_version" >> "$temp_file"
+done
+
+# format and display
+column -t -s $'\t' "$temp_file"
+
+# remove the temporary file
+rm -f "$temp_file"
